0ef8894eb6f1909cceb367007907abcc519d69a4
Dan Vanderkam <danvdk@gmail.com>
2020-12-01 10:00:33 -0500

    Day 1 star 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,17 +1,27 @@
 /*
 const NUMS: [i32; 6] = [
-  1721,
-  979,
-  366,
-  299,
-  675,
-  1456
+
 ];
 */
 
-use super::super::util;
+use aoc2020::util;
+
+fn read_ints(path: &str) -> Vec<i32> {
+  let mut out: Vec<i32> = Vec::new();
+  let lines = util::read_lines(path).unwrap();
+
+  for line in lines {
+    let num = line.unwrap().parse::<i32>().unwrap();
+    out.push(num);
+  }
+
+  out
+}
 
 fn main() {
+  let nums = read_ints("inputs/day1.txt");
+  println!("Read {} nums", nums.len());
+
   for num1 in nums.iter() {
     for num2 in nums.iter() {
       if num1 + num2 == 2020 {
