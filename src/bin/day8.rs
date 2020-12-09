19ce3cb528006cc791c1ffb1059a51199d075336
Dan Vanderkam <danvdk@gmail.com>
2020-12-08 19:48:49 -0500

    switch to enum for Op

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,34 +1,41 @@
 #[macro_use]
 extern crate lazy_static;
-// use std::collections::HashMap;
 use aoc2020::util;
 use regex::Regex;
 use std::{collections::HashSet, env};
 
-// TODO:   Change Instruction.op to be more like a tagged union
+// TODO: x Change Instruction.op to be more like a tagged union
 // TODO: x Use match / case on op.op
 // TODO: x Use if let in process_file
 
 #[derive(PartialEq, Eq, Clone, Debug)]
-struct Instruction {
-    op: String,
-    arg: i32,
+enum Op {
+    Nop(i32),
+    Acc(i32),
+    Jmp(i32),
 }
 
 lazy_static! {
     static ref INSTR_RE: Regex = Regex::new(r"^([a-z]+) ([-+])([0-9]+)$").unwrap();
 }
 
-fn parse_instruction(text: &str) -> Instruction {
+fn parse_instruction(text: &str) -> Op {
     let groups = INSTR_RE.captures(text).unwrap();
-    let op = String::from(&groups[1]);
+    let op = &groups[1];
     let sgn = if &groups[2] == "-" { -1 } else { 1 };
     let arg = sgn * groups[3].parse::<i32>().unwrap();
 
-    Instruction { op, arg }
+    match op {
+        "nop" => Op::Nop(arg),
+        "acc" => Op::Acc(arg),
+        "jmp" => Op::Jmp(arg),
+        _ => {
+            panic!("Unknown op: {}", op);
+        }
+    }
 }
 
-fn read_program(path: &str) -> Vec<Instruction> {
+fn read_program(path: &str) -> Vec<Op> {
     util::read_lines(path)
         .unwrap()
         .map(|line| line.unwrap())
@@ -37,7 +44,7 @@ fn read_program(path: &str) -> Vec<Instruction> {
 }
 
 /// Returns the final accumulator value or None if the program goes into an infinite loop.
-fn run_program(ops: &Vec<Instruction>) -> Option<i32> {
+fn run_program(ops: &Vec<Op>) -> Option<i32> {
     let mut line = 0usize;
     let mut acc = 0;
     let mut run_lines: HashSet<usize> = HashSet::new();
@@ -56,16 +63,13 @@ fn run_program(ops: &Vec<Instruction>) -> Option<i32> {
         let mut next_line = line + 1;
         run_lines.insert(line);
         let op = &ops[line];
-        match op.op.as_str() {
-            "nop" => {}
-            "acc" => {
-                acc += op.arg;
+        match op {
+            Op::Nop(_arg) => {}
+            Op::Acc(arg) => {
+                acc += arg;
             }
-            "jmp" => {
-                next_line = (line as i32 + op.arg) as usize;
-            }
-            _ => {
-                panic!("Surprise op: {:?}", op);
+            Op::Jmp(arg) => {
+                next_line = (line as i32 + arg) as usize;
             }
         }
         line = next_line;
@@ -76,17 +80,11 @@ fn process_file(path: &str) {
     let program = read_program(path);
     for (i, op) in program.iter().enumerate() {
         let mut variation = program.to_vec();
-        if op.op == "nop" {
-            variation[i] = Instruction {
-                op: String::from("jmp"),
-                arg: op.arg,
-            };
-        } else if op.op == "jmp" {
-            variation[i] = Instruction {
-                op: String::from("nop"),
-                arg: op.arg,
-            };
-        }
+        variation[i] = match op {
+            Op::Nop(arg) => Op::Jmp(*arg),
+            Op::Jmp(arg) => Op::Nop(*arg),
+            Op::Acc(arg) => Op::Acc(*arg),
+        };
         if let Some(acc) = run_program(&variation) {
             println!("swap: {}, acc: {}", i, acc);
             break;
@@ -110,20 +108,7 @@ mod tests {
 
     #[test]
     fn test_parse_instr() {
-        assert_eq!(
-            parse_instruction("nop +0"),
-            Instruction {
-                op: String::from("nop"),
-                arg: 0
-            }
-        );
-
-        assert_eq!(
-            parse_instruction("jmp -4"),
-            Instruction {
-                op: String::from("jmp"),
-                arg: -4
-            }
-        );
+        assert_eq!(parse_instruction("nop +0"), Op::Nop(0));
+        assert_eq!(parse_instruction("jmp -4"), Op::Jmp(-4));
     }
 }
