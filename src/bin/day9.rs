0c68ca0564b46aa4666f5a8aeecf83c4a67403ca
Dan Vanderkam <danvdk@gmail.com>
2020-12-09 09:17:41 -0500

    day 9 star 2

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -8,24 +8,40 @@ fn is_pair_sum(n: u64, nums: &[u64]) -> bool{
     nums.iter().combinations(2).any(|x| x[0] + x[1] == n)
 }
 
-fn process_file(path: &str, preamble_len: i32) {
-    let nums: Vec<u64> = read_lines(path)
-        .unwrap()
-        .map(|line| line.unwrap().parse::<u64>().unwrap())
-        .collect();
+fn parse_file(path: &str) -> Vec<u64> {
+    read_lines(path)
+    .unwrap()
+    .map(|line| line.unwrap().parse::<u64>().unwrap())
+    .collect()
+}
 
+fn process_file(nums: &[u64], preamble_len: i32) -> Option<u64> {
     let mut i = preamble_len as usize;
     loop {
         i += 1;
         if i >= nums.len() {
-            break;
+            return None;
         }
 
         let n = nums[i];
         if !is_pair_sum(n, &nums[(i as i32 - preamble_len) as usize..i]) {
             println!("Invalid: {}", n);
+            return Some(n);
         }
     }
+}
+
+fn find_sequence(nums: &[u64], n: u64) {
+    let m = (0..nums.len()).combinations(2).find(|x| {
+        nums[x[0]..=x[1]].iter().sum::<u64>() == n
+    });
+    let indices = m.unwrap();
+    let a = indices[0];
+    let b = indices[1];
+    let s = &nums[a..=b];
+    let lo = s.iter().min().unwrap();
+    let hi = s.iter().max().unwrap();
+    println!("a: {}, b: {}, min: {}, max: {}, sum: {}", a, b, lo, hi, lo + hi);
 }
 
 fn main() {
@@ -35,8 +51,10 @@ fn main() {
     }
 
     let preamble = args[2].parse::<i32>().unwrap();
+    let nums = parse_file(&args[1]);
 
-    process_file(&args[1], preamble);
+    let invalid = process_file(&nums, preamble).unwrap();
+    find_sequence(&nums, invalid);
 }
 
 #[cfg(test)]
