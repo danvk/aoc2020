6aa1a87e49c6ed5720d82275197ee69622f38201
Dan Vanderkam <danvdk@gmail.com>
2020-12-12 07:33:11 -0500

    Day 12 star 2

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -5,6 +5,8 @@ use std::{env, fmt};
 struct Ship {
     x: i32,
     y: i32,
+    wdx: i32,
+    wdy: i32,
     /// degrees, 0 = east
     dir: i32,
 }
@@ -45,22 +47,45 @@ fn dir(degrees: i32) -> (i32, i32) {
     }
 }
 
+fn rot(d: (i32, i32), degrees: i32) -> (i32, i32) {
+    let mut degrees = ((degrees % 360) + 360) % 360;
+    let (mut dx, mut dy) = d;
+
+    while degrees > 0 {
+        degrees -= 90;
+        let ndx = -dy;
+        let ndy = dx;
+        dx = ndx;
+        dy = ndy;
+    }
+    assert_eq!(0, degrees);
+
+    (dx, dy)
+}
+
 fn process_file(path: &str) {
-    let mut ship = Ship { x: 0, y: 0, dir: 0 };
+    let mut ship = Ship { x: 0, y: 0, wdx: 10, wdy: 1, dir: 0 };
     for line in util::read_lines(path).unwrap().map(|line| line.unwrap()) {
         let action = parse_action(&line);
 
         match action {
-            Action::N(arg) => ship.y += arg,
-            Action::S(arg) => ship.y -= arg,
-            Action::E(arg) => ship.x += arg,
-            Action::W(arg) => ship.x -= arg,
-            Action::L(arg) => ship.dir += arg,
-            Action::R(arg) => ship.dir -= arg,
+            Action::N(arg) => ship.wdy += arg,
+            Action::S(arg) => ship.wdy -= arg,
+            Action::E(arg) => ship.wdx += arg,
+            Action::W(arg) => ship.wdx -= arg,
+            Action::L(arg) => {
+                let (dx, dy) = rot((ship.wdx, ship.wdy), arg);
+                ship.wdx = dx;
+                ship.wdy = dy;
+            },
+            Action::R(arg) => {
+                let (dx, dy) = rot((ship.wdx, ship.wdy), -arg);
+                ship.wdx = dx;
+                ship.wdy = dy;
+            },
             Action::F(arg) => {
-                let (dx, dy) = dir(ship.dir);
-                ship.x += arg * dx;
-                ship.y += arg * dy;
+                ship.x += arg * ship.wdx;
+                ship.y += arg * ship.wdy;
             }
         }
         println!("{:?} -> {:?}", &action, &ship);
@@ -90,4 +115,27 @@ mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
+    #[test]
+    fn test_mod() {
+        let x = -270 - 360;
+        assert_eq!(-270, x % 360);
+
+        assert_eq!(90, ((x % 360) + 360) % 360);
+    }
+
+    #[test]
+    fn test_rot() {
+        assert_eq!((2, 1), rot((2, 1), 0));
+        assert_eq!((-1, 2), rot((2, 1), 90));
+        assert_eq!((-2, -1), rot((2, 1), 180));
+        assert_eq!((1, -2), rot((2, 1), 270));
+        assert_eq!((1, -2), rot((2, 1), -90));
+        assert_eq!((-2, -1), rot((2, 1), -180));
+        assert_eq!((-1, 2), rot((2, 1), -270));
+        assert_eq!((2, 1), rot((2, 1), 360));
+        //        (dx, dy)
+        // +90 -> (-dy, dx)
+        // +180 -> (-dx, -dy)
+        // +270 -> (dy, -dx)
+    }
 }
