1814b4e5254478021ea7ed41d9ffc35680f916e3
Dan Vanderkam <danvdk@gmail.com>
2020-12-13 08:32:49 -0500

    first_congruence

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,23 +1,88 @@
 use aoc2020::util;
-use std::{env, fmt};
+use std::{env};
+
+/// Find the first n such that:
+///   n = t1 (mod p1)
+///   n = t2 (mod p2)
+///
+/// Subsequent ns will be n = n + k*p1*p2
+fn first_congruence(p1: u64, t1: u64, p2: u64, t2: u64) -> u64 {
+    if p1 < p2 {
+        return first_congruence(p2, t2, p1, t1);
+    }
+    for k1 in 1..=p2 {
+        let n = k1 * p1 + t1;
+        if n % p2 == t2 {
+            return n;
+        }
+    }
+    panic!("Unable to find congruence: {} {}", p1, p2);
+}
 
 fn process_file(path: &str) {
     let mut lines = util::read_lines(path).unwrap();
-    let t0 = lines.next().unwrap().unwrap().parse::<u32>().unwrap();
-    let primes = lines
+    let t0 = lines.next().unwrap().unwrap().parse::<u64>().unwrap();
+    let mut tp = lines
         .next()
         .unwrap()
         .unwrap()
         .split(",")
         .filter(|&x| x != "x")
-        .map(|x| x.parse::<u32>().unwrap())
+        .map(|x| x.parse::<u64>().unwrap())
+        .enumerate()
+        .map(|(rem, p)| (p, rem))
         .collect::<Vec<_>>();
+    tp.sort();
+    tp = tp.into_iter().rev().collect();
 
     println!("t0: {}", t0);
-    println!("primes: {:?}", primes);
+    println!("primes: {:?}", tp);
 
-    let (wait, p) = primes.iter().map(|&p| ((p - (t0 % p)), p)).min().unwrap();
-    println!("Answer (part 1): {} * {} = {}", wait, p, wait * p);
+    for w in tp.windows(2) {
+        // Keep getting "refutable pattern in binding" errors here:
+        // let &[(p1, t1), (p2, t2)] = w;
+        let (p1, t1) = w[0];
+        let (p2, t2) = w[1];
+        println!("base ({}, {}) = {}", p1, p2, first_congruence(p1, t1 as u64, p2, t2 as u64));
+    }
+
+    let n = first_congruence(59 * 31, 592, 19 * 13, 118);
+    println!("fc 592, 118 = {}", n);
+    println!("fc 7 next = {}", first_congruence(7, 0, 59 * 31 * 19 * 13, n))
+    // primes: [(59, 2), (31, 3), (19, 4), (13, 1), (7, 0)]
+    // base (59, 31) = 592
+    // base (31, 19) = 251
+    // base (19, 13) = 118
+    // base (13, 7) = 14
+    // 181663
+
+    // let (wait, p) = primes.iter().map(|&p| ((p - (t0 % p)), p)).min().unwrap();
+    // println!("Answer (part 1): {} * {} = {}", wait, p, wait * p);
+
+    // t = 0 (mod p1)
+    // t = 1 (mod p2)
+    // -> t = 0 (mod p1 * p2)
+    // -> p1 * t = p1 (mod p2)
+
+    // Find the first one, then subsequent ones will be by adding p1 * p2
+    // 11 * 59 + 2 = 651 = 1 (mod 13)
+    // 24 * 59 + 2 = 1418 = 1 (mod 13)
+    // 37 * 59 + 2 = 2185 = 1 (mod 13)
+    // 50 * 59 + 2 = 2952 = 1 (mod 13)
+    // 63 * 59 + 2 = 3719 = 1 (mod 13)
+    // 76 * 59 + 2 = 4486 = 1 (mod 13)
+    // 89 * 59 + 2 = 5253 = 1 (mod 13)
+
+    // t = 0 (mod 7)
+    // t = 1 (mod 13)
+    // t = 2 (mod 59)
+    // t = 3 (mod 31)
+    // t = 4 (mod 19)
+
+    // t = 59k2 + 2
+    // 7k1 = 59k2 + 2
+    //
+
     /*
     for p in primes {
         let last = p * (t0 / p);
@@ -44,4 +109,9 @@ fn main() {
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
+
+    #[test]
+    fn test_find_congruence() {
+        assert_eq!(first_congruence(59, 2, 13, 1), 651);
+    }
 }
