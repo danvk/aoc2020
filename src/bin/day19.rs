2b0abbd17b7a5b5c0d05423328c904dff431f04e
Dan Vanderkam <danvdk@gmail.com>
2020-12-19 14:54:07 -0500

    expand + test

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,8 +1,10 @@
 #[macro_use]
 extern crate lazy_static;
 use regex::Regex;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::env;
+use itertools::Itertools;
+use std::iter::FromIterator;
 
 #[derive(Debug)]
 enum Rule {
@@ -51,8 +53,8 @@ impl Rule {
 lazy_static! {
     // 3: 4 5 | 5 4
     // 4: "a"
-    static ref LITERAL_RE: Regex = Regex::new(r#"^(\d+): "([a-z])"$"#).unwrap();
-    static ref PATTERN_RE: Regex = Regex::new(r#"^(\d+): (.*)$"#).unwrap();
+    static ref LITERAL_RE: Regex = Regex::new(r#"^ *(\d+): "([a-z])"$"#).unwrap();
+    static ref PATTERN_RE: Regex = Regex::new(r#"^ *(\d+): (.*)$"#).unwrap();
 }
 
 fn parse_rule(rule: &str) -> (i32, Rule) {
@@ -79,12 +81,68 @@ fn parse_rules(rules: &str) -> HashMap<i32, Rule> {
     rules.split('\n').map(|rule| parse_rule(rule)).collect::<HashMap<_, _>>()
 }
 
-fn process_file(path: &str) {
+fn match_part2(text: &str, rules: &HashMap<i32, Rule>) -> bool {
+    let mut non42 = Some(text);  // bit of text left after matching rule 42
+    let rule42 = &rules[&42];
+    let mut paired31s: Vec<i32> = vec![];
+    loop {
+        if let Some(rest) = rule42.match_str(non42.unwrap(), rules, "") {
+            println!("Matched a 42! Down to {}", rest);
+            if rest.is_empty() {
+                return true;  // matches rule 8: all 42s!
+            }
+            paired31s.push(31);
+            let r = Rule::Pattern(vec![paired31s.clone()]);
+            if let Some(rest31) = r.match_str(rest, rules, "") {
+                if rest31.is_empty() {
+                    return true;  // matches rule 11: equal number of 42s and 31s
+                }
+            }
+            non42 = Some(rest);
+        } else {
+            return false;
+        }
+    }
+}
+
+fn expand_rule(rule: &Rule, rules: &HashMap<i32, Rule>) -> HashSet<String> {
+    let mut s = HashSet::new();
+    match rule {
+        Rule::Literal(c) => { s.insert(c.to_string()); },
+        Rule::Pattern(pats) => {
+            for pat in pats {
+                let mut pieces = pat.iter().map(|i| expand_rule(&rules[i], rules)).collect::<Vec<_>>();
+                let mut poss = pieces.pop().unwrap();
+                for piece in pieces {
+                    let x = poss.iter().cartesian_product(piece.iter());
+                    poss = x.map(|(a, b)| {
+                        let mut s = a.to_owned();
+                        s.push_str(b);
+                        s
+                    }).collect();
+                }
+                s.extend(poss);
+            }
+        },
+    }
+    s
+}
+
+fn process_file(path: &str, part: &str) {
     let contents = std::fs::read_to_string(path).unwrap();
     let chunks = contents.split("\n\n").collect::<Vec<_>>();
     assert_eq!(2, chunks.len());
 
-    let rules = parse_rules(&chunks[0]);
+    let mut rules = parse_rules(&chunks[0]);
+
+    if part == "2" {
+        println!("Swapping for part 2");
+        rules.insert(8, Rule::Pattern(vec![vec![42], vec![42, 8]]));
+        rules.insert(11, Rule::Pattern(vec![vec![42, 31], vec![42, 11, 31]]));
+    }
+
+    let rules = rules;
+
     println!("Rules: {:?}", rules);
     let rule0 = rules.get(&0).unwrap(); // why can't I do rules[0] here?
 
@@ -102,11 +160,11 @@ fn process_file(path: &str) {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        panic!("Expected one argument, got {}: {:?}", args.len(), args);
+    if args.len() != 3 {
+        panic!("Expected two arguments, got {}: {:?}", args.len(), args);
     }
 
-    process_file(&args[1]);
+    process_file(&args[1], &args[2]);
 }
 
 
@@ -147,5 +205,83 @@ mod tests {
         // assert_eq!(None, rule0.match_str("bababa", &rules));
         // assert_eq!(None, rule0.match_str("aaabbb", &rules));
         // assert_eq!(None, rule0.match_str("aaaabbb", &rules));
+    }
+
+    #[test]
+    fn test2() {
+        let rules = parse_rules(r#"42: 9 14 | 10 1
+        9: 14 27 | 1 26
+        10: 23 14 | 28 1
+        1: "a"
+        11: 42 31
+        5: 1 14 | 15 1
+        19: 14 1 | 14 14
+        12: 24 14 | 19 1
+        16: 15 1 | 14 14
+        31: 14 17 | 1 13
+        6: 14 14 | 1 14
+        2: 1 24 | 14 4
+        0: 8 11
+        13: 14 3 | 1 12
+        15: 1 | 14
+        17: 14 2 | 1 7
+        23: 25 1 | 22 14
+        28: 16 1
+        4: 1 1
+        20: 14 14 | 1 15
+        3: 5 14 | 16 1
+        27: 1 6 | 14 18
+        14: "b"
+        21: 14 1 | 1 14
+        25: 1 1 | 1 14
+        22: 14 14
+        8: 42
+        26: 14 22 | 1 20
+        18: 15 15
+        7: 14 5 | 1 21
+        24: 14 1"#);
+        // let rule0 = &rules[&0];
+        println!("rules: {:?}", rules);
+
+        assert!(match_part2("bbabbbbaabaabba", &rules));
+        //                        bbaabaabba
+        //                             aabba
+
+        // assert_eq!(Some(""), rule0.match_str("bbabbbbaabaabba", &rules, ""));
+        // assert_eq!(Some(""), rule0.match_str("babbbbaabbbbbabbbbbbaabaaabaaa", &rules, ""));
+        // assert_eq!(Some(""), rule0.match_str("babbbbaabbbbbabbbbbbaabaaabaaa", &rules, ""));
+    }
+
+    #[test]
+    fn test_expand() {
+        let rules = parse_rules(r#"0: 4 1 5
+        1: 2 3 | 3 2
+        2: 4 4 | 5 5
+        3: 4 5 | 5 4
+        4: "a"
+        5: "b""#);
+        // TODO: find a better way to write HashSet<String> literals
+        assert_eq!(expand_rule(&rules[&4], &rules), HashSet::from_iter(vec![String::from("a")]));
+        assert_eq!(
+            expand_rule(&rules[&3], &rules),
+            HashSet::from_iter(vec![String::from("ab"), String::from("ba")])
+        );
+        assert_eq!(
+            expand_rule(&rules[&2], &rules),
+            HashSet::from_iter(vec![String::from("aa"), String::from("bb")])
+        );
+        assert_eq!(
+            expand_rule(&rules[&1], &rules),
+            HashSet::from_iter(vec![
+                String::from("aaab"),
+                String::from("aaba"),
+                String::from("bbab"),
+                String::from("bbba"),
+                String::from("abaa"),
+                String::from("abbb"),
+                String::from("baaa"),
+                String::from("babb"),
+            ])
+        );
     }
 }
