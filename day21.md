1dfd71ba430f933e4c4ca52bb6cd280857b7c23f
Dan Vanderkam <danvdk@gmail.com>
2020-12-21 17:38:00 -0500

    Day 21 star 1

---
+++ day21.md
@@ -0,0 +1,54 @@
+Each allergen is found in exactly one ingredient.
+Each ingredient contains zero or one allergen.
+Allergens aren't always marked.
+Even if an allergen isn't listed, the ingredient that contains that allergen could still be present
+
+4             mxmxvkd      sbzzf sqjhc     (contains fish)
+1       kfcds mxmxvkd nhms       sqjhc     (contains dairy, fish)
+2 fvjkl       mxmxvkd      sbzzf       trh (contains dairy)
+3 fvjkl                          sqjhc     (contains soy)
+
+1   K M N    SQ   (contains dairy, fish)
+2 F   M   SB    T (contains dairy)
+3 F          SQ   (contains soy)
+4     M   SB SQ   (contains fish)
+  2 1 3 1  2  3 1
+
+SB
+ 1: does not contain dairy, fish
+ 3: does not contain soy
+
+K
+ 2: cannot contain dairy
+ 3: cannot contain soy
+ 4: cannot contain fish
+ If you're only in rule 1, you cannot be an allergen
+
+If you're not in a rule that produces an allergen, you can't contain that allergen
+
+N
+ (same logic as K, can't be an allergen)
+
+T
+  3: does not contain soy
+  4: does not contain fish
+  1: does not contain dairy
+
+4&1: M SQ contains fish
+1&2: M    contains dairy
+
+ F = fvjkl
+ K = kfcds
+ M = mxmxvkd
+ N = nhms
+SB = sbzzf
+SQ = sqjhc
+ T = trh
+
+kfcds, nhms, sbzzf, or trh cannot contain an allergen
+K      N     SB        T
+-> F, M, SQ can
+allergens = fish, dairy, soy
+
+Sample: 7 ingredients, 3 allergens
+Input: 200 ingredients, 8 allergens
--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,48 +1,69 @@
-// Each allergen is found in exactly one ingredient.
-// Each ingredient contains zero or one allergen.
-// Allergens aren't always marked.
-// Even if an allergen isn't listed, the ingredient that contains that allergen could still be present
-
-// 4             mxmxvkd      sbzzf sqjhc     (contains fish)
-// 1       kfcds mxmxvkd nhms       sqjhc     (contains dairy, fish)
-// 2 fvjkl       mxmxvkd      sbzzf       trh (contains dairy)
-// 3 fvjkl                          sqjhc     (contains soy)
-
-// fvjkl
-// kfcds
-// mxmxvkd
-// nhms
-// sbzzf
-// sqjhc
-// trh
-
-// kfcds, nhms, sbzzf, or trh cannot contain an allergen
-
+#[macro_use]
+extern crate lazy_static;
 use aoc2020::util;
+use regex::Regex;
+use std::{collections::{HashMap, HashSet}, env};
 
-fn read_ints(path: &str) -> Vec<i32> {
-  let mut out: Vec<i32> = Vec::new();
-  let lines = util::read_lines(path).unwrap();
 
-  for line in lines {
-    let num = line.unwrap().parse::<i32>().unwrap();
-    out.push(num);
-  }
+lazy_static! {
+  // mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+  static ref RECIPE_RE: Regex = Regex::new(r#"^ *([a-z ]+) \(contains (.*)\)$"#).unwrap();
+}
 
-  out
+fn parse_recipe(recipe: &str) -> (HashSet<String>, HashSet<String>) {
+  let caps = RECIPE_RE.captures(recipe).unwrap();
+  (
+    caps[1].split(" ").map(|x| String::from(x)).collect(),
+    caps[2].split(", ").map(|x| String::from(x)).collect()
+  )
+}
+
+fn process_file(path: &str) {
+    let recipes = util::read_lines(path).unwrap().map(|line| parse_recipe(&line.unwrap())).collect::<Vec<_>>();
+
+    let mut all_ingredients = HashSet::new();
+    let mut all_allergens = HashSet::new();
+    for (ings, alls) in recipes.iter() {
+      all_ingredients.extend(ings.iter());
+      all_allergens.extend(alls.iter());
+    }
+    let ingredients = all_ingredients.iter().map(|i| *i).collect::<Vec<_>>();
+    let allergens = all_allergens.iter().map(|a| *a).collect::<Vec<_>>();
+
+    println!("{} Ingredients, {} allergens", ingredients.len(), allergens.len());
+
+    let mut answer = 0;
+    for &ing in ingredients.iter() {
+      let mut excluded = HashSet::new();
+      let mut num_app = 0;
+      for (ings, alls) in recipes.iter() {
+        if !ings.contains(ing) {
+          excluded.extend(alls.iter());
+        } else {
+          num_app += 1;
+        }
+      }
+      if excluded.len() == allergens.len() {
+        println!("Excluded {}", ing);
+        answer += num_app;
+      }
+    }
+
+    println!("Answer: {}", answer);
 }
 
 fn main() {
-  let nums = read_ints("inputs/day1.txt");
-  println!("Read {} nums", nums.len());
-
-  for num1 in nums.iter() {
-    for num2 in nums.iter() {
-      for num3 in nums.iter() {
-        if num1 + num2 + num3 == 2020 {
-          println!("{} * {} * {} = {}", num1, num2, num3, num1 * num2 * num3);
-        }
-      }
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Expected one argument, got {}: {:?}", args.len(), args);
     }
-  }
+
+    process_file(&args[1]);
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
 }
