5ba41b8d993bc90e503c7955e89b2a8cf43e8265
Dan Vanderkam <danvdk@gmail.com>
2020-12-23 12:19:13 -0500

    cleanup for star 2

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -1,14 +1,6 @@
-use std::{collections::{HashMap, HashSet}, fmt, time::Instant};
+use std::{fmt, time::Instant};
 use std::env;
-use aoc2020::set;
 use itertools::Itertools;
-
-// 162.99 promo rate, three months --> 212.99 after
-// cancelation fee is $15/month for remaining months of contract
-// can do install on January 8th
-// between 8 AM and 11 AM time slot (probably closer to 8, first slot of the day)
-// 574-360-7063
-// pin is 4268
 
 struct Cups {
     // 0 is empty
@@ -17,14 +9,6 @@ struct Cups {
 }
 
 impl Cups {
-    fn max_index(&self) -> usize {
-        self.nexts.len() - 1
-    }
-
-    fn remove(&mut self, i: usize) -> () {
-        self.nexts[i] = self.nexts[self.nexts[i]];
-    }
-
     fn remove_three_after(&mut self, c0: usize) -> (usize, usize, usize) {
         let c1 = self.nexts[c0];
         let c2 = self.nexts[c1];
@@ -55,15 +39,12 @@ impl Cups {
         self.find_dest(dest, (c1, c2, c3))
     }
 
-    // 30 minutes of long distance is $30
-    // unlimited is $42.99
-
     fn play_one_round(&mut self) -> () {
-        println!("cups: {}", self);
+        // println!("cups: {}", self);
         let (c1, c2, c3) = self.remove_three_after(self.current);
-        println!("pick up: {}, {}, {}", c1, c2, c3);
+        // println!("pick up: {}, {}, {}", c1, c2, c3);
         let dest = self.find_dest(self.current, (c1, c2, c3));
-        println!("destination: {}", dest);
+        // println!("destination: {}", dest);
 
         self.insert_after(dest, c3);
         self.insert_after(dest, c2);
@@ -79,55 +60,46 @@ impl fmt::Display for Cups {
         let mut s = String::with_capacity(self.nexts.len() * 2 + 2);
         s.push_str(&format!("({})", self.current));
         let mut next = self.nexts[self.current];
-        while next != self.current {
+        let mut x = 0;
+        while next != self.current && x < 25 {
             s.push_str(" ");
             s.push_str(&format!("{}", next));
             next = self.nexts[next];
+            x += 1;
         }
         write!(f, "{}", s)
     }
 }
 
 fn play_game(nums: Vec<usize>, num_rounds: usize) {
-    /*
-    let m = nums.iter().max().unwrap();
-    let mut v: Vec<i32> = Vec::with_capacity(1_000_000);
-    for n in &nums {
-        v.push(*n);
-    }
-    for n in (m+1)..=1_000_000 {
-        v.push(n);
-    }
-    println!("cups len: {}", v.len());
-    // naive: 12 hours
-
-    let mut cups = Cups { current: 0, cups: v };
-    */
-    let mut nexts = vec![0; nums.len() + 1];
+    let mut nexts = vec![0; 1_000_001];
     for (&v0, &v1) in nums.iter().zip(nums.iter().skip(1)) {
         nexts[v0] = v1;
     }
-    nexts[*nums.last().unwrap()] = *nums.first().unwrap();
-    println!("nexts: {:?}", nexts);
+    nexts[*nums.last().unwrap()] = nums.len() + 1;
+
+    for n in nums.len() + 1..=1_000_000 {
+        nexts[n] = n + 1;
+    }
+    nexts[1_000_000] = nums[0];
 
     let mut cups = Cups { current: nums[0] as usize, nexts };
     println!("Cups: {}", cups);
 
     let now = Instant::now();
-    for i in 1..=num_rounds {
-        println!("-- move {}--", i);
+    for _i in 1..=num_rounds {
+        // println!("-- move {}--", i);
         cups.play_one_round();
-        // let x = cups.index_of(1).unwrap();
-        // if i % 100 == 0 {
-        //     println!("{} two right of 1: {}, {}",
-        //         i,
-        //         cups.at(x as i32 + 1),
-        //         cups.at(x as i32 + 2),
-        //     );
-        // }
     }
     let elapsed_ms = now.elapsed().as_millis();
     println!("{} rounds in {}ms = {}ms/round", num_rounds, elapsed_ms, elapsed_ms as f64 / num_rounds as f64);
+
+    println!("cups: {}", cups);
+    let c1 = cups.nexts[1];
+    let c2 = cups.nexts[c1];
+    println!("two right of 1: {} * {} = {}",
+        c1, c2, c1 * c2,
+    );
 
     // println!("-- final --");
     // println!("cups: {}", cups);
