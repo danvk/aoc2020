a08ef4ab0318e199aedae45918215ddc7c9d0d69
Dan Vanderkam <danvdk@gmail.com>
2020-12-24 14:56:12 -0500

    got a bug somewhere

--- src/bin/day24.rs
+++ src/bin/day24.rs
@@ -20,9 +20,11 @@ enum HexDir {
     NE,
 }
 
-fn hexmove(pos: (i32, i32), dir: &HexDir) -> (i32, i32) {
+const DIRS: [HexDir; 6] = [HexDir::E, HexDir::SE, HexDir::SW, HexDir::W, HexDir::NW, HexDir::NE];
+
+fn hexmove(pos: &(i32, i32), dir: &HexDir) -> (i32, i32) {
     use HexDir::*;
-    let (x, y) = pos;
+    let &(x, y) = pos;
     match dir {
         E => (x + 1, y),
         W => (x - 1, y),
@@ -40,7 +42,7 @@ fn hexmoves(moves: &[HexDir]) -> (i32, i32) {
     //     pos = hexmove(pos, dir);
     //     println!("{:?} -> {:?}", dir, pos);
     // }
-    moves.iter().fold((0, 0), |pos, dir| hexmove(pos, dir))
+    moves.iter().fold((0, 0), |pos, dir| hexmove(&pos, dir))
     // pos
 }
 
@@ -72,8 +74,57 @@ fn parse_line(line: &str) -> Vec<HexDir> {
     r
 }
 
-fn process_file(path: &str) {
-    let mut tiles: HashMap<(i32, i32), bool> = HashMap::new();
+// Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
+
+type TileFloor = HashMap<(i32, i32), bool>;
+// false = white
+// true = black
+
+fn remove_false(floor: &mut TileFloor) -> () {
+    let whites = floor.iter().filter(|(_, v)| **v).map(|(k, _)| *k).collect_vec();
+    for k in whites {
+        floor.remove(&k);
+    }
+}
+
+fn neighbors(pos: &(i32, i32)) -> Vec<(i32, i32)> {
+    DIRS.iter().map(|d| hexmove(pos, d)).collect_vec()
+}
+
+fn num_neighbors(floor: &TileFloor, pos: &(i32, i32)) -> usize {
+    DIRS.iter().map(|d| hexmove(pos, d)).filter(|p| *floor.get(p).unwrap_or(&false)).count()
+}
+
+fn next_day(floor: TileFloor) -> TileFloor {
+    let mut next: TileFloor = HashMap::new();
+    for (pos, &v) in floor.iter() {
+        if v {
+            // Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white.
+            let nn = num_neighbors(&floor, pos);
+            if nn == 2 || nn > 2 {
+                next.insert(*pos, true);
+            } else {
+                next.insert(*pos, false);
+            }
+
+            for n in neighbors(pos) {
+                if !*floor.get(&n).unwrap_or(&false) && !next.contains_key(&n) {
+                    // Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
+                    next.insert(*pos, num_neighbors(&floor, &n) == 2);
+                }
+            }
+        }
+    }
+
+    next
+}
+
+fn num_black(floor: &TileFloor) -> usize {
+    floor.values().filter(|&v| *v).count()
+}
+
+fn process_file(path: &str, num_days: usize) {
+    let mut tiles: TileFloor = HashMap::new();
     for line in aoc2020::util::read_lines(path).unwrap().map(|line| line.unwrap()) {
         let m = parse_line(&line);
         let (x, y) = hexmoves(&m);
@@ -81,22 +132,29 @@ fn process_file(path: &str) {
         tiles.insert((x, y), !old_tile);
     }
 
-    println!("Grid: {:?}", tiles);
-    println!("num black: {}", tiles.values().filter(|&v| *v).count());
+    // println!("Grid: {:?}", tiles);
+    println!("num black: {}", num_black(&tiles));
+
+    for day in 1..=num_days {
+        tiles = next_day(tiles);
+        println!("Day {}: {}", day, num_black(&tiles));
+    }
 }
 
 fn main() {
     let args = env::args().collect_vec();
-    if args.len() != 2 {
-        panic!("Expected two argument, got {}: {:?}", args.len(), args);
+    if args.len() != 3 {
+        panic!("Expected two arguments, got {}: {:?}", args.len(), args);
     }
 
-    process_file(&args[1]);
+    process_file(&args[1], args[2].parse::<usize>().unwrap());
 }
 
 
 #[cfg(test)]
 mod tests {
+    use aoc2020::map;
+
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
@@ -113,12 +171,51 @@ mod tests {
         assert_eq!((0, 0), hexmoves(&parse_line("nwwswee")));
     }
 
-//    NW, W, SW, E, E
+    // [HexDir::E, HexDir::SE, HexDir::SW, HexDir::W, HexDir::NW, HexDir::NE];
+    #[test]
+    fn test_neighbors() {
+        assert_eq!(neighbors(&(0, 0)), vec![
+            (1, 0),
+            (1, 1),
+            (0, 1),
+            (-1, 0),
+            (0, -1),
+            (1, -1),
+        ]);
+    }
+
+    #[test]
+    fn test_num_neighbors() {
+        assert_eq!(num_neighbors(&map!{
+            (1, 0) => true,
+            (1, 1) => true,
+            (0, 1) => true,
+            (-1, 0) => true,
+            (0, -1) => true,
+            (1, -1) => true
+        }, &(0, 0)), 6);
+
+        assert_eq!(num_neighbors(&map!{
+            (1, 0) => true,
+            (1, 1) => true,
+            (1, -1) => true
+        }, &(0, 0)), 3);
+
+        assert_eq!(num_neighbors(&map!{
+            (1, 0) => true,
+            (1, 1) => true,
+            (0, 1) => true,
+            (0, -1) => false,
+            (1, -1) => true
+        }, &(0, 0)), 4);
+    }
+
+//  y NW, W, SW, E, E
 // -1 -1 0 1 2 3 4
 //  0  -1 0 1 2 3 4
-//  1  -1 0 1 2 3 4
-//  2 -1 0 1 2 3 4
-//  3  -1 0 1 2 3 4
+//  1 -1 0 1 2 3 4
+//  2  -1 0 1 2 3 4
+//  3 -1 0 1 2 3 4
 }
 
 // ideas:
